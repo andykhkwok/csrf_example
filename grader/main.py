5a0fb7388b9139756493874f78a536673caf2399
Andy Kwok <andykhkwok@gmail.com>
2019-09-22 15:22:57 -0700

    Activity 8

--- grader/main.py
+++ grader/main.py
@@ -1,22 +1,33 @@
 import os
 import base64
+import random
+import string
+import sys
 
-from flask import Flask, request
+from flask import Flask, request, session
 from model import Grade 
 
 app = Flask(__name__)
+app.secret_key = b'\x98E\xb3\xb4O\xd9\xd8\xf7\xe1C\x89\xef\x89\xc2G\xd6\xa9W\x84\xc2hCP\xfb'
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    #Added
 
+    if 'csrf_token' not in session:
+        letters = string.ascii_lowercase
+        key = ''.join(random.choice(letters) for i in range(10))
+        session['csrf_token'] = key 
+        
     if request.method == 'POST':
-        g = Grade(
-            student=request.form['student'],
-            assignment=request.form['assignment'],
-            grade=request.form['grade'],
-        )
-        #print("(" + request.form['grade'] + ")")
-        g.save()
+        if request.form.get('csrf_token', None) == session['csrf_token']:   #Added
+            g = Grade(
+                student=request.form['student'],
+                assignment=request.form['assignment'],
+                grade=request.form['grade'],
+            )
+            #print("(" + request.form['grade'] + ")")
+            g.save()
 
     body = """
 <html>
@@ -33,12 +44,16 @@ def home():
 
     <label for="grade">Grade</label>
     <input type="text" name="grade"><br>
+    
+    <input type="hidden" name="csrf_token" value="{}"> 
 
     <input type="submit" value="Submit">
 </form>
 
 <h2>Existing Grades</h2>
-"""
+""".format(session['csrf_token']) #Added
+
+#SHOWING
     
     for g in Grade.select():
         body += """
